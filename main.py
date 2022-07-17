62a227ba67ad0b04e60831c34289899e8e84db59
Dealer327 <88377783+Dealer327@users.noreply.github.com>
2022-07-17 13:04:37 +0300

    добавления супер цикла

---
+++ main.py
@@ -0,0 +1,13 @@
+"""Модуль верхнего уровня для учебного проекта крестики-нолики"""
+
+    # Импорт дополнительных модулей проекта
+    # TODO: import
+
+
+    #супер цикл
+    while True:
+        command = input('>')
+        if command in ('quit', 'exit', 'q', 'e'):
+            break
+        elif command in ('new', 'n'):
+            pass
