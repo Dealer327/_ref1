712cbae6d4473fe5a3da06f538ec61d286401397
Dealer327 <88377783+Dealer327@users.noreply.github.com>
2022-07-20 16:51:34 +0300

    правки и обновления файлов проекта

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 """Модуль верхнего уровня для учебного проекта крестики-нолики"""
 
-# FIXME: убрать лишний отступ в последующих строках;
-#        по завершении, данный комментарий удалить
-    # Импорт дополнительных модулей проекта
-    # TODO: import
+
+# Импорт дополнительных модулей проекта
+# TODO: import
 
 
-    # суперцикл
-    while True:
-        command = input('>')
-        if command in ('quit', 'exit', 'q', 'e'):
-            break
-        elif command in ('new', 'n'):
-            pass
+# суперцикл
+while True:
+    command = input('>')
+    if command in ('quit', 'exit', 'q', 'e'):
+        break
+    elif command in ('new', 'n'):
+        pass
