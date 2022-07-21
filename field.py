1b3511b2e9d856e85ef5302a158f794b8d104fd3
Dealer327 <88377783+Dealer327@users.noreply.github.com>
2022-07-21 15:45:47 +0300

    реализация функции вывода игрового поля и хода игрока

--- field.py
+++ field.py
@@ -1,8 +1,27 @@
+'''Модуль реализации функции о сделаных ходах в текущей партии'''
 
-def show_field():
+
+'''Тестовая переменая для отоброжения ячеек игрового поля'''
+turns = ['','','','','','','','','']
+'''Функция вывода игрового поля и отоброжения текущих ходов '''
+def show_field(turns):
+    print('-'*17)
+    print(f"|  {turns[0]}  |  {turns[1]}   |  {turns[2]}  |")
+    print('-' * 19)
+    print(f"|  {turns[3]}  |  {turns[4]}   |  {turns[5]}  |")
+    print('-' * 19)
+    print(f"|  {turns[6]}  |  {turns[7]}   |  {turns[8]}  |")
     print('-'*19)
-    for i in range(3):
-        print(f"|     |     |     |")
-        print('-'*19)
 
-print(show_field())
+'''Функция ввода хода игрока !!!тест'''
+
+def turns_test():
+    a = int(input())
+    while a != '':
+        if a > 1 and  a < 9:
+            for i in range(len(turns)):
+                if i == a - 1:
+                    turns[i] = 'x'
+                    show_field(turns)
+        a = int(input())
+turns_test()
