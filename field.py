70e7bba2e71114d5d4791359b7b2f15098afaf35
Dealer327 <88377783+Dealer327@users.noreply.github.com>
2022-07-28 21:05:16 +0300

    добавления функции хранение ходов в текущей партии

--- field.py
+++ field.py
@@ -1,22 +1,27 @@
 """Модуль реализации функции о сделанных ходах в текущей партии"""
 
+def turnsgame():
+    """Функция хранения ходов в текущей партии"""
+    turns = [' ' for _ in range(9)]
+    player_pos = {'X' : [], 'O' : []}
+    return turns
 
-turns = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+turns = turnsgame()
 
-def show_field():
+
+def show_field(turns):
     """Функция отрисовки игрового поля"""
+
     print("\n")
     print("\t     |     |")
     print("\t  {}  |  {}  |  {}".format(turns[0], turns[1], turns[2]))
     print('\t_____|_____|_____')
-
     print("\t     |     |")
     print("\t  {}  |  {}  |  {}".format(turns[3], turns[4], turns[5]))
     print('\t_____|_____|_____')
-
     print("\t     |     |")
-
     print("\t  {}  |  {}  |  {}".format(turns[6], turns[7], turns[8]))
     print("\t     |     |")
     print("\n")
-show_field()
+
+show_field(turns)
