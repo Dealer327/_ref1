6c5d8908f4810e4a0f3fbf6affacef18f02a7a11
Gennadiy <renardes@gmail.com>
2022-08-05 10:13:55 +0300

    правки улучшения читаемости

--- field.py
+++ field.py
@@ -1,34 +1,34 @@
 """Модуль реализации функции о сделанных ходах в текущей партии"""
 
 
-
-
 DIM = 3
 turns = [[' ']*DIM for _ in range(DIM)]
 
 
-
 def show_field(playfiled):
     """Вывод игрового поля"""
-    #Весь функционал вывода поля будет пополняться в ходе написания остального кода
+    # весь функционал вывода поля будет пополняться в ходе написания остального кода
     #!!!ДОБАВИТЬ НЕ ЗАБЫТЬ!!!
     for i in range(DIM):
         print('|'.join([str(row).center(5) for row in playfiled[i]]))
         print('-'*17)
 
+
 def check_win():
-    """Проверка выйграшных ходов"""
-    #столбцы игрового поля
+    """Проверка выигрышных ходов"""
+    # столбцы игрового поля
     horizontals = [[turns[j][i] for j in range(DIM)] for i in range(DIM)]
-    #диагональные линии игровго поля
+    # диагональные линии игрового поля
     diagonals = [[turns[i][i] for i in range(DIM)],
                  [turns[i][DIM-i-1] for i in range(DIM)]]
-    #перебор горизонтальных вертикальных и диаганальных линий игровго поля
-    for vin in (turns,horizontals,diagonals):
-        for row in vin:
+    # перебор горизонтальных вертикальных и диагональных линий игрового поля
+    for win in (turns, horizontals, diagonals):
+        for row in win:
             if ' ' in row:
                 continue
             elif len(set(row)) == 1 and all(row):
                 return True
+
+
 show_field(turns)
 check_win()
