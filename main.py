3d3cbbf5863afdc105b339b6e37baf7c8d16494f
Dealer327 <88377783+Dealer327@users.noreply.github.com>
2022-08-06 14:47:15 +0300

    функция ввода имени игрока и добавления информации о нем в файл конфигураций

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 """Модуль верхнего уровня для учебного проекта крестики-нолики"""
 
+import config
+from configparser import ConfigParser as CP
 
-# Импорт дополнительных модулей проекта
-# TODO: import
+
+def name_player():
+    """Ввод имени игрока и проверка присутствие этого имени в глобальной переменной."""
+
+    name = input('Введите свой ник -> ')
+    # Не правильно отробатывает скрипт, каждый раз перезаписывает значения!
+    # ИСПРАВИТЬ
+    if name not in config.PLAYERS:
+        config_file = CP()
+        config_file[name] = {'first_time': 'False', 'stats': '0,0,0'}
+
+        with open('players.ini', 'w') as file_obj:
+            config_file.write(file_obj)
+        print(config.PLAYERS)
 
 
+name_player()
+print(config.read_ini())
 # суперцикл
 while True:
     command = input('>')
