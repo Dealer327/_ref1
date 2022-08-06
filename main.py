10e76d396abeef8639698978573035700b47c5a3
Dealer327 <88377783+Dealer327@users.noreply.github.com>
2022-08-06 15:00:05 +0300

    функция ввода имени

--- main.py
+++ main.py
@@ -8,15 +8,22 @@ def name_player():
     """Ввод имени игрока и проверка присутствие этого имени в глобальной переменной."""
 
     name = input('Введите свой ник -> ')
-    # Не правильно отробатывает скрипт, каждый раз перезаписывает значения!
-    # ИСПРАВИТЬ
+    # Не правильно отрабатывает скрипт, каждый раз перезаписывает значения!
+    # не знаю как ИСПРАВИТЬ!
     if name not in config.PLAYERS:
-        config_file = CP()
-        config_file[name] = {'first_time': 'False', 'stats': '0,0,0'}
+        '''1ВАРИАНТ config_file = CP()
+        config_file.add_section(name)
+        config_file.set(name, 'first_time', 'False')
+        config_file.set(name, 'stats', '0,0,0')
+        with open('players.ini', 'w') as file_obj:
+            config_file.write(file_obj)
+        print(config.PLAYERS)'''
+
+        '''2 ВАРИАНТ config_file[name] = {'first_time': 'False', 'stats': '0,0,0'}
 
         with open('players.ini', 'w') as file_obj:
             config_file.write(file_obj)
-        print(config.PLAYERS)
+        print(config.PLAYERS)'''
 
 
 name_player()
